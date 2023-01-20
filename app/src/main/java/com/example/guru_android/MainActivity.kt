a612dd5497d23669b6a9622e24362f4c6feb2d82
hswest <precious01133@gmail.com>
2023-01-20 11:56:37 +0900

    메인 수정

--- app/src/main/java/com/example/guru_android/MainActivity.kt
+++ app/src/main/java/com/example/guru_android/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.guru_android
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var date : TextView
+    lateinit var selectDate : ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
